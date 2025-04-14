5b1437e578f4f89a34b669ce55047daf8d10774d
Cpiggott-lab <crpapc@gmail.com>
2025-04-14 15:35:43 +0200

    day 1 update

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,37 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+  moveToNextQuestion() {
+    if (this.currentQuestionIndex < this.questions.length - 1) {
+      this.currentQuestionIndex += 1;
+    }
+  }
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      const randomIndex = Math.floor(Math.random() * (i + 1));
+      const temp = this.questions[i];
+      this.questions[i] = this.questions[randomIndex];
+      this.questions[randomIndex] = temp;
+    }
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  checkAnswer(answer) {
+    if (this.getQuestion().answer === answer) {
+      this.correctAnswers += 1;
+      return true;
+    }
+    return false;
+  }
 
-    // 4. shuffleQuestions()
-
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
+  hasEnded() {
+    return this.currentQuestionIndex >= this.questions.length;
+  }
 }
